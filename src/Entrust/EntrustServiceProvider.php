793154b0c2b356e1016ba616e22bee2b3280ebe5
Szomy <szomy1@gmail.com>
2016-10-21 17:45:12 +0200

    Working entrust

--- src/Entrust/EntrustServiceProvider.php
+++ src/Entrust/EntrustServiceProvider.php
@@ -61,29 +61,29 @@ class EntrustServiceProvider extends ServiceProvider
     {
         // Call to Entrust::hasRole
         \Blade::directive('role', function($expression) {
-            return "<?php if (\\Entrust::hasRole{$expression}) : ?>";
+            return "<?php if (\\Entrust::hasRole({$expression})) : ?>";
         });
 
         \Blade::directive('endrole', function($expression) {
-            return "<?php endif; // Entrust::hasRole ?>";
+            return "<?php endif; // Entrust::hasRole() ?>";
         });
 
         // Call to Entrust::can
         \Blade::directive('permission', function($expression) {
-            return "<?php if (\\Entrust::can{$expression}) : ?>";
+            return "<?php if (\\Entrust::can({$expression})) : ?>";
         });
 
         \Blade::directive('endpermission', function($expression) {
-            return "<?php endif; // Entrust::can ?>";
+            return "<?php endif; // Entrust::can() ?>";
         });
 
         // Call to Entrust::ability
         \Blade::directive('ability', function($expression) {
-            return "<?php if (\\Entrust::ability{$expression}) : ?>";
+            return "<?php if (\\Entrust::ability({$expression})) : ?>";
         });
 
         \Blade::directive('endability', function($expression) {
-            return "<?php endif; // Entrust::ability ?>";
+            return "<?php endif; // Entrust::ability() ?>";
         });
     }
 
